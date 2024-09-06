0f26160bef99510982c4dbe6d6e4c51c82239510
Asya0 <anya.gerasimenko763@mail.ru>
2024-09-06 22:40:53 +0600

    feat: add vite & navigation

---
+++ brand-page/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default App;
