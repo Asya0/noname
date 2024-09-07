25eb39faebd7330100992429f99bf0fb39886d10
Asya0 <anya.gerasimenko763@mail.ru>
2024-09-07 22:02:18 +0600

    feat: add nav styles

--- brand-page/src/App.jsx
+++ brand-page/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-// import './App.css'
+import "./App.css";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <li href="#">About</li>
           <li href="#">Contact</li>
         </ul>
+        <button>login</button>
       </nav>
     </>
   );
