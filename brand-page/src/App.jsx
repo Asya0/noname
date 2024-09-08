df9d32dab5dd39d5c52cc56da8fcc0b070b115e2
Asya0 <anya.gerasimenko763@mail.ru>
2024-09-08 22:23:15 +0600

    feat: add hero section

--- brand-page/src/App.jsx
+++ brand-page/src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <>
-      <nav>
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="logo" />
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-        <button>login</button>
-      </nav>
+      <Navigation />
+      <HeroSection />
     </>
   );
 }
